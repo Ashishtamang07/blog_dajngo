2ffef049d296b1454c86a80b9be5ab601c9de42f
ashish tamang <tamangashish314@gmail.com>
2024-04-16 12:11:13 +0545

    pagination,  author page with posts

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
+from django.contrib.auth.models import User
 from blog.models import Post
 from django.views.generic import ListView , DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
@@ -16,7 +17,23 @@ class PostListView(ListView):
     template_name= 'blog/home.html' #<app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted'] # order by latest post 
-    paginate_by=2
+    paginate_by=5
+    
+class UserPostListView(ListView):
+    model = Post
+    template_name= 'blog/user_posts.html' #<app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted'] # order by latest post 
+    paginate_by=5
+    
+    """
+    get user from url with usename 
+    if user exist filter post by author= user
+    """
+    def get_queryset(self):
+        user= get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+        
     
 class PostDetailView(DetailView):
     model = Post
@@ -42,12 +59,10 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin ,UpdateView):
     def form_valid(self, form):
         form.instance.author= self.request.user
         return super().form_valid(form)
-    "only the author of post can ipdate post"
+    "only the author of post can update post"
     def test_func(self):
         post= self.get_object()
-        if self.request.user == post.author:
-            return True
-        return False
+        return self.request.user == post.author
     
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin,DeleteView):
     model = Post
